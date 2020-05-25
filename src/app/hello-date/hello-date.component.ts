aced2acfe61aa0b8235ee07a88db835589097744
varun-maraka <varun.maraka@gmail.com>
2020-05-24 19:02:38 -0500

    Interpolation and one way data binding. Calling method in a double curly

--- src/app/hello-date/hello-date.component.ts
+++ src/app/hello-date/hello-date.component.ts
@@ -7,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloDateComponent implements OnInit {
   message: string = "hello from TypeScript";
+  dateMsg: string;
   currTime: Date = new Date();
   currDate: string = new Date().toDateString();
   seccons: string =  this.currTime.getHours().toString()  + ":" +this.currTime.getMinutes().toString() + ":" + this.currTime.getSeconds().toString() ;
-  constructor() { }
+  constructor() { 
+    setInterval(()=> {
+      let currentDate = new Date();
+      this.dateMsg = currentDate.toDateString() + ':' + currentDate.toLocaleTimeString() ;
+    },1000)
+    
+  }
   ngOnInit(): void {
+  }
+
+  addTwoNumbers(a: number, b: number){
+    return a+b;
   }
 }
